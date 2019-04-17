4cb180bccb08b992caf99069ace0245ff5247e25
Javid Alizada <javid_alizada@epam.com>
2019-04-17 13:54:00 +0000

    S3_With_CloudFront

--- S3_With_CloudFront/main.tf
+++ S3_With_CloudFront/main.tf
@@ -2,11 +2,14 @@
 provider "aws" "ep_aws" {
 	region = "us-east-1"
 }
+
+#Storage Module
 module "ep_storage" {
 	source = "./storage"
 	ep_bucket_name = "${var.ep_bucket}"
 }
 
+#Security Module
 module "ep_security" {
 	source = "./security"
 	ep_www_site_id = "${module.ep_storage.ep_www_site}"
